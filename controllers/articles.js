0c49604752363e4964d866436eb8d6f5da8a1022
Tanel Pauls <Tanel.Pauls@voco.ee>
2025-02-07 09:52:59 +0200

    As a user I can see single yoga article with author data. See #3

--- controllers/articles.js
+++ controllers/articles.js
@@ -13,9 +13,14 @@ const getAllarticles = (req, res) => {
 const getArticleBySlug = (req, res) => {
   let sql = `SELECT * FROM article WHERE slug='${req.params.slug}'`;
   db.query(sql, (error, result) => {
-    res.render('article',{
-      article: result
-    })
+    const article = result[0];
+    const author_id = result[0].author_id;
+    const sql = `SELECT * FROM author WHERE id="${author_id}"`;
+    db.query(sql, (error, result) => {
+      const author = result[0];
+      article["author_name"] = author.name;
+      res.render("article", { article: article });
+    });
   });
 };
 
